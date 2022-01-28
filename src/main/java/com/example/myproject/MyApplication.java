68f4949e15580ec7d0ddfc6100f34665b742ac68
Phillip Webb <pwebb@vmware.com>
2022-01-27 16:06:04 -0800

    Add custom sanitizer

--- src/main/java/com/example/myproject/MyApplication.java
+++ src/main/java/com/example/myproject/MyApplication.java
@@ -1,15 +1,26 @@
 package com.example.myproject;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.endpoint.SanitizingFunction;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.CommandLinePropertySource;
 
 @SpringBootApplication
 @EnableConfigurationProperties(GreetingProperties.class)
 public class MyApplication {
 
+	@Bean
+	public SanitizingFunction sanitizingFunction() {
+		return data -> (data.getPropertySource().getName()
+				.equals(CommandLinePropertySource.COMMAND_LINE_PROPERTY_SOURCE_NAME))
+						? data.withValue("you're never gonna get it")
+						: data;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
 	}
-	
+
 }
