433d5c58e3b3b403794d3ebf48fcf6cccff6de6d
Phillip Webb <pwebb@vmware.com>
2022-01-27 16:01:47 -0800

    Drop @ConstructorBinding

--- src/main/java/com/example/myproject/GreetingProperties.java
+++ src/main/java/com/example/myproject/GreetingProperties.java
@@ -4,7 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 @ConfigurationProperties("greeting")
-@ConstructorBinding
 public record GreetingProperties(String salutation) {
 
 }
